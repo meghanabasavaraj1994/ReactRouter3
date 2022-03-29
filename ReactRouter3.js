7ef8f47aeaa0097cf9f014742134f8d2884a2bd3
meghanabasavaraj1994 <102538632+meghanabasavaraj1994@users.noreply.github.com>
2022-03-29 22:51:01 +0530

    Add files via upload

---
+++ ReactRouter3.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App4() {
+  return (
+    <Router>
+      <div>
+        <Link to="/">Main page</Link> | <Link to="/page1">Page 1</Link>|{" "}
+        <Link to="/page2">Page 2</Link>
+      </div>
+
+      <Switch>
+        <Route path="/page2">
+          <h1>I'm Page 2</h1>
+        </Route>
+        <Route path="/page1">
+          <h1>I'm Page 1</h1>
+        </Route>
+        <Route path="/">
+          <h1>I'm Main page</h1>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App4;
